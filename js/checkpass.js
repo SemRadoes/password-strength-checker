95a8690493242f1370efc87539d1507a9e86d8dd
Sem Radoes <semradijs@hotmail.com>
2022-07-12 14:06:34 +0200

    needs styling

--- js/checkpass.js
+++ js/checkpass.js
@@ -1,19 +1,20 @@
-const passwordCheck = document.querySelector("#password").value;
-if(passwordCheck.length >= 5 && passwordCheck.length <= 20){
-    if(passwordCheck.match(/^(?=.*[-+_!@#$%^&*., ?])/)){
-        if(passwordCheck.match(/^(?=.*[a-z])(?=.*[A-Z])/)){
-            if(passwordCheck.match(/(?=.*[0-9])/)){
-                document.querySelector('#succesdiv').style.display = "block";
+const form = document.querySelector('#validateForm');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const passwordCheck = document.querySelector("#password").value;
+    if(passwordCheck.length >= 5 && passwordCheck.length <= 20){
+        if(passwordCheck.match(/^(?=.*[-+_!@#$%^&*., ?])/)){
+            if(passwordCheck.match(/^(?=.*[a-z])(?=.*[A-Z])/)){
+                if(passwordCheck.match(/(?=.*[0-9])/)){
+                    document.querySelector('#succesdiv').style.display = "block";
+                    setTimeout(() => {document.querySelector('#succesdiv').style.display = "none"},1500);
+                } 
             } 
-        } 
+        }
+    }  else {
+        document.querySelector('#errordiv').style.display = "block";
+        setTimeout(() => {document.querySelector('#errordiv').style.display = "none"},1500);
     }
-}  else {
-    
-}
+})
 
-     
-  
-
-  function showDiv(selector, color){
-    document.querySelector(selector).style.color = color;
-     }
